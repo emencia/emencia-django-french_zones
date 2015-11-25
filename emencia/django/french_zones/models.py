72404af5c9cd3796170acbe2a2323345cfb0ed75
Julien MIOTTE <mike.perdide@perdide.yt>
2015-11-25 16:47:12 +0100

    Adding Region2016 to models and updating Region and Departments models

--- emencia/django/french_zones/models.py
+++ emencia/django/french_zones/models.py
@@ -2,7 +2,8 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
-class Region(models.Model):
+
+class Region2016(models.Model):
     code = models.CharField(_('code INSEE'), max_length=2, primary_key=True)
     name = models.CharField(_('official name'), max_length=128)
     slug = models.SlugField(max_length=128, blank=True)
@@ -14,9 +15,27 @@ class Region(models.Model):
         verbose_name = _('region')
         verbose_name_plural = _('regions')
         ordering = ('name',)
+        db_table = "french_zones_region_2016"
+
+
+class Region(models.Model):
+    code = models.CharField(_('code INSEE'), max_length=2, primary_key=True)
+    name = models.CharField(_('official name'), max_length=128)
+    slug = models.SlugField(max_length=128, blank=True)
+    region_2016 = models.ForeignKey(Region2016, null=True)
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _('region')
+        verbose_name_plural = _('regions')
+        ordering = ('name',)
+
 
 class Department(models.Model):
     region = models.ForeignKey(Region, verbose_name=_('region'))
+    region_2016 = models.ForeignKey(Region2016, null=True)
 
     code = models.CharField(_('code'), max_length=3, primary_key=True)
     name = models.CharField(_('official name'), max_length=128)
@@ -29,6 +48,3 @@ class Department(models.Model):
         verbose_name = _('department')
         verbose_name_plural = _('departments')
         ordering = ('code',)
-
-
-
