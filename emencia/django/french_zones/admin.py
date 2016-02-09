567d19ce6b44e7713a7f1829f787624235cfbd52
Lafaye Philippe <lafaye@emencia.com>
2016-02-09 17:31:02 +0100

    add admin view for region2016

--- emencia/django/french_zones/admin.py
+++ emencia/django/french_zones/admin.py
@@ -3,12 +3,13 @@ from django.contrib import admin
 from django.utils.translation import ugettext as _
 
 from emencia.django.french_zones.models import Region
+from emencia.django.french_zones.models import Region2016
 from emencia.django.french_zones.models import Department
 
 class DepartmentInline(admin.StackedInline):
     model = Department
 
-class RegionAdmin(admin.ModelAdmin):
+class Region2016Admin(admin.ModelAdmin):
     search_fields = ('name', 'code')
     list_display = ('name', 'code', 'departments')
     fields = ('code', 'name',)
@@ -19,6 +20,10 @@ class RegionAdmin(admin.ModelAdmin):
     def departments(self, region):
         return ', '.join([d.name for d in region.department_set.all()])
 
+class RegionAdmin(Region2016Admin):
+    list_display = ('name', 'code', 'region_2016', 'departments')
+    fields = ('code', 'name', 'region_2016')
+
 class DepartmentAdmin(admin.ModelAdmin):
     search_fields = ('name', 'code')
     list_display = ('name', 'code', 'region')
@@ -26,6 +31,7 @@ class DepartmentAdmin(admin.ModelAdmin):
     actions_on_top = False
     actions_on_bottom = True
 
+admin.site.register(Region2016, Region2016Admin)
 admin.site.register(Region, RegionAdmin)
 admin.site.register(Department, DepartmentAdmin)
 
